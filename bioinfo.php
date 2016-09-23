8f44f0e25e092359c72c6a47d8305700419fb54c
kanhu-pcr-code <kcm.eid@gmail.com>
2016-09-23 07:30:29 +0530

    style sheets

--- bioinfo.php
+++ bioinfo.php
@@ -2,10 +2,27 @@
  include_once("db_conf.php");
  include_once("header_sect.php");
  ?>
+<div class="sect1"> 
+  <p class="heading1" >Bioinformatics</p>
+The field of bioinformatics has evolved such that the most pressing task now involves the analysis and interpretation of various types of data.
+<img src="img/test_img.png" alt="test alignment: shows a lot of this" width="50px" height="100px"/>
+ This includes nucleotide and amino acid sequences, protein domains, and protein structures.[14] The actual process of analyzing and interpreting data is referred to as computational biology. Important sub-disciplines within bioinformatics and computational biology include:
+<ul>
+    <li>Development and implementation of computer programs that enable efficient access to, use and management of, various types of information.     </li>
+    <li> Development of new algorithms (mathematical formulas) and statistical measures that assess relationships among members of large data sets. For example, there are methods to locate a gene within a sequence, to predict protein structure and/or function, and to cluster protein sequences into families of related sequences.     </li>
+</ul>
+</div>
+
+<div class="boxed_item">
+  <span >Bioinformatics2</span>
+ The field of bioinformatics has evolved such that the most pressing task now involves the analysis and interpretation of various types of data. This includes nucleotide and amino acid sequences, protein domains, and protein structures.[14] The actual process of analyzing and interpreting data is referred to as computational biology. Important sub-disciplines within bioinformatics and computational biology include:
+<ul>
+    <li>Development and implementation of computer programs that enable efficient access to, use and management of, various types of information.     </li>
+    <li> Development of new algorithms (mathematical formulas) and statistical measures that assess relationships among members of large data sets. For example, there are methods to locate a gene within a sequence, to predict protein structure and/or function, and to cluster protein sequences into families of related sequences.     </li>
+</ul>
+ </div>
  
- 
- 
- 
+  
  
  
  <?php
--- db_conf.php
+++ db_conf.php
@@ -1,6 +1,7 @@
 <?php
-$page= str_replace('/cloudnotes/','',$_SERVER['REQUEST_URI']);
-
+#$page= str_replace('/cloudnotes/','',$_SERVER['REQUEST_URI']);
+$page= explode('/',$_SERVER['REQUEST_URI']);
+$page=end($page);
 $servername = "127.0.0.1";
 $username = "b15_18636880";
 $password = "643j879n";
@@ -11,3 +12,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 ?> 
+
